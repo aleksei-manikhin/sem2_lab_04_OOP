8e1b83f4367c9dab5bb9f5b92d1226a2e878eae9
aleksei-manikhin <138869287+aleksei-manikhin@users.noreply.github.com>
2026-05-26 13:42:44 +0300

    Version!

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,6 +12,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle("CSV Surface Viewer");
+    setWindowIcon(ui->icon_2->icon());
 
     SetupFacade();
     SetupConnections();
